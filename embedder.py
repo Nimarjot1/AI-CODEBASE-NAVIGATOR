8ffb6d03c7be888fb078293209da351f399bea5d
Nimarjot kaur <157558006+Nimarjot1@users.noreply.github.com>
2026-03-18 16:07:02 +0530

    Update embedder.py

--- embedder.py
+++ embedder.py
@@ -1,10 +1,7 @@
-from dotenv import load_dotenv
-import os
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
 
-load_dotenv()
 
 def chunk_code_files(code_files):
 
@@ -17,13 +14,25 @@ def chunk_code_files(code_files):
 
     for file in code_files:
         content = file["content"]
+        file_path = file["file_path"]
+
+        # 🔥 metadata tagging
+        file_type = "normal"
+
+        if "lib/router" in file_path:
+            file_type = "router"
+        elif "lib/application" in file_path:
+            file_type = "application"
+        elif "lib/" in file_path:
+            file_type = "core"
 
         split_chunks = splitter.split_text(content)
 
         for chunk in split_chunks:
             chunks.append({
-                "file_path": file["file_path"],
-                "content": chunk
+                "file_path": file_path,
+                "content": chunk,
+                "file_type": file_type
             })
 
     return chunks
@@ -31,12 +40,15 @@ def chunk_code_files(code_files):
 
 def create_vector_store(chunks):
 
-    texts = []
-    metadata = []
+    texts = [chunk["content"] for chunk in chunks]
 
-    for chunk in chunks:
-        texts.append(chunk["content"])
-        metadata.append({"file_path": chunk["file_path"]})
+    metadata = [
+        {
+            "file_path": chunk["file_path"],
+            "file_type": chunk["file_type"]
+        }
+        for chunk in chunks
+    ]
 
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
