b80d4736a41d28f4e5341617559b4f7bc6d7f19c
Nimarjot kaur <157558006+Nimarjot1@users.noreply.github.com>
2026-03-18 16:08:16 +0530

    Update test_loader.py

--- test_loader.py
+++ test_loader.py
@@ -2,42 +2,20 @@ from repo_loader import clone_repo, load_code_files
 from embedder import chunk_code_files, create_vector_store
 from rag_engine import create_rag_chain
 
-
-# ---------------------------
-# STEP 1: Load Repo
-# ---------------------------
 repo = clone_repo("https://github.com/expressjs/express")
 
 files = load_code_files(repo)
 
-print("Number of code files:", len(files))
-
-
-# ---------------------------
-# STEP 2: Chunk Code
-# ---------------------------
 chunks = chunk_code_files(files)
 
 print("Number of chunks:", len(chunks))
 
-
-# ---------------------------
-# STEP 3: Create Vector Store
-# ---------------------------
 vector_store = create_vector_store(chunks)
 
 print("Vector store created successfully")
 
-
-# ---------------------------
-# STEP 4: Create RAG Engine
-# ---------------------------
 qa = create_rag_chain(vector_store)
 
-
-# ---------------------------
-# STEP 5: Ask Questions (Loop 🔥)
-# ---------------------------
 while True:
     question = input("\nAsk a question (or type 'exit'): ")
 
